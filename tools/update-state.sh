95c729ba69d1d0f8089274c07c554c8a55807123
Graham Christensen <graham@grahamc.com>
2025-06-17 10:35:01 -0400

    Add the magic nix cachebinaryrevision tothe state file

--- tools/update-state.sh
+++ tools/update-state.sh
@@ -11,7 +11,7 @@ default_branch() {
 }
 
 get_latest_revision() {
-    gh api "repos/$REPO/commits/$(default_branch)" \
+    gh api "repos/$1/commits/$(default_branch)" \
         | jq -r '.sha'
 }
 
@@ -24,6 +24,20 @@ checkout_tag() {
         --json isLatest,tagName
 }
 
+magic_nix_cache_binary_rev() {
+  sha=$(get_latest_revision "DeterminateSystems/magic-nix-cache")
+
+  (
+    echo "Checking to see if $sha for magic-nix-cache has binaries yet..."
+    for arch in ARM64-macOS X64-macOS X64-Linux ARM64-Linux; do
+      curl -LI --fail \
+        https://install.determinate.systems/magic-nix-cache/rev/$sha/$arch
+    done
+  ) >&2
+
+  echo "$sha"
+}
+
 determinate_nix_action_major() {
     gh release list \
         --repo DeterminateSystems/determinate-nix-action \
@@ -34,12 +48,14 @@ determinate_nix_action_major() {
 }
 
 main() {
-    revision=$(get_latest_revision)
+    revision=$(get_latest_revision "$REPO")
     checkout_tag=$(checkout_tag)
+    mnc_binary_rev=$(magic_nix_cache_binary_rev)
 
     jq -n '$ARGS.named' \
         --arg upstream_action_revision "$revision" \
         --arg "source_tag" "$MAGIC_NIX_CACHE_TAG" \
+        --arg "binary_revision" "$mnc_binary_rev" \
         --arg "checkout_action_tag" "$checkout_tag" \
         | cat > tools/state.json
 }
